975a3e553bceccc5836f397956f1b7cfedc89ed2
JeppeRonnow <jepperw@gmail.com>
2026-05-06 21:12:53 +0200

    Fix sentence case in settings description

--- src/settings.ts
+++ src/settings.ts
@@ -35,7 +35,7 @@ export class PDFPageEmbedderSettingTab extends PluginSettingTab {
 		new Setting(containerEl)
 			.setName("Use Obsidian native PDF viewer")
 			.setDesc(
-				"Generate ![[pdf#page=x]] syntax instead of custom code blocks, which is not recommended as it is slower and offers less control.",
+				"Generate standard Obsidian links instead of custom code blocks for better compatibility.",
 			)
 			.addToggle((toggle) =>
 				toggle
